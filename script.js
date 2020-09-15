0176e176530844caf9f9fd02dc30528f60fcfac6
Coogii <chasemckinney70@gmail.com>
2020-09-15 13:33:16 -0500

    Initial commit

---
+++ script.js
@@ -0,0 +1,7 @@
+const favor = document.getElementById("favor");
+let numOfFavor = 0;
+function clickFunction2() {
+    numOfFavor += 1;
+    favor.innerHTML = "favor" + numOfFavor;
+   }
+console.log(1);
